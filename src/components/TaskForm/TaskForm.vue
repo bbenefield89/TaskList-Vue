65208d23942583ddc5c379ed7fb481f5ddcf1b37
bbenefield89 <bsquared18@gmail.com>
2019-01-30 16:51:44 -0500

    REMOVED COMPONENTS AND ADD STYLES
    - src/components/TaskForm/TaskForm.vue
      - Possibly temporarily removed input components and used DOM elements instead
        - need to figure out how to properly remove text from input after submitting task (only applicable if text input is a Vue Component and not a DOM element)
      - Add some styles using 'SCSS'

--- src/components/TaskForm/TaskForm.vue
+++ src/components/TaskForm/TaskForm.vue
@@ -1,33 +1,69 @@
 <template>
-  <form @submit="createTask" >
-    <TaskFormInput type="text" v-on:update-task-name-input-value="updateTaskNameInputValue" />
-    <TaskFormInput type="submit" />
-  </form>
+  <div>
+    <form class="task_form" @submit="createTask" >
+      <input class="task_form__text_input" type="text" v-model="taskName" placeholder="New Task Name" />
+      <input class="task_form__submit_input" type="submit" value="Create Task 🛫" />
+    </form>
+  </div>
 </template>
 
 <script>
-  import TaskFormInput from './components/TaskFormInput/TaskFormInput'
-
   export default {
     name: 'TaskForm',
-    components: { TaskFormInput },
-    methods: {
-      createTask(e) {
-        e.preventDefault()
-        this.$emit('create-task', this.taskName)
-      },
-      updateTaskNameInputValue(taskNameFromChildInput) {
-        this.taskName = taskNameFromChildInput
-      }
-    },
+
     data() {
       return {
         taskName: ''
       }
-    }
+    },
+
+    methods: {
+      createTask(e) {
+        e.preventDefault()
+        this.$emit('create-task', this.taskName)
+        this.taskName = ''
+      },
+
+      updateTaskNameInputValue(taskNameFromChildInput) {
+        this.taskName = taskNameFromChildInput
+      }
+    },
   }
 </script>
 
-<style>
+<style lang="scss" scoped>
+  div {
+    display: flex;
+    justify-content: center;
 
+    .task_form {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-evenly;
+      width: 25%;
+      
+      .task_form__text_input {
+        border: none;
+        border-bottom: 1px solid;
+        margin-bottom: 0.5rem;
+        padding: 6px;
+      }
+
+      .task_form__submit_input {
+        border: none;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.3);
+        cursor: pointer;
+        font-size: 20px;
+        outline: none;
+        padding: 6px;
+        position: relative;
+        transform: all 0.15s;
+
+        &:active {
+          box-shadow: inset 0px 2px 4px rgba(0, 0, 0, 0.3);
+          top: 4px;
+        }
+      }
+    }
+  }
 </style>
